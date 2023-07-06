bcef9667515101980d8f07094ce4f8e7cf2b4201
Veny Kniazev <venykniazev@Venys-MacBook-Air.local>
2023-07-06 19:01:15 +0300

    create rust project

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use rand::Rng;
+fn main() {
+    let mut range = rand::thread_rng();
+    let rnum: i32 = range.gen_range(1..=100);
+
+    if rnum % 3 == 0 {
+         println!("Fizz");
+
+    } else if rnum % 5 == 0 {
+        println!("Buzz");
+
+    } else if rnum % 15 == 0 {
+         println!("FizzBuzz");
+
+    } else {
+        println!("Nothing");
+    }
+println!("Представленное число {}", rnum);
+    
+}
